8fe0eff226296e3eea21b8f621b1d331a72f8b97
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-07 05:18:27 +0600

    add include/config.sample.php

---
+++ include/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database configuration
+define('DB_HOST', 'localhost'); // Database host
+
+define('DB_NAME', 'user_authentication'); // Database name
+
+define('DB_USER', 'root'); // Change if necessary
+
+define('DB_PASS', ''); // Change if necessary
+?>
